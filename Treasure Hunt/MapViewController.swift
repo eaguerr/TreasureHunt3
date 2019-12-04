415ddcc371885d6d7b347660881f07b15ec7b189
Eduard Guerra <eaguerra@students.tulsatech.edu>
2019-12-04 10:49:48 -0600

    added map annotations

--- Treasure Hunt/MapViewController.swift
+++ Treasure Hunt/MapViewController.swift
@@ -17,8 +17,30 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
           super.viewDidLoad()
-        
+        createAnnotations(locations: annotationLocations)
     }
+    
+    let annotationLocations = [
+        ["title": "Riverside Cache", "latitude": 36.033846, "longitude": -95.981797],
+        ["title": "Peoria Cache", "latitude": 36.210999, "longitude": -95.976343],
+        ["title": "Broken Arrow Cache", "latitude": 36.003395, "longitude": -95.833936],
+        ["title": "Sand Springs Cache", "latitude": 36.138147, "longitude": 96.095937],
+        ["title": "Owasso Cache", "latitude": 36.314425, "longitude": 95.818798],
+        ["title": "Lemley Memorial Cache", "latitude": 36.113343, "longitude": 95.886945]
+    ]
+    
+    func createAnnotations(locations: [[String : Any]]) {
+        for location in locations {
+            let annotations = MKPointAnnotation()
+            annotations.title = location["title"] as? String
+            annotations.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees, longitude: location["longitude"] as! CLLocationDegrees)
+            
+            mapView.addAnnotation(annotations)
+        }
+    }
+    
+    
+    
     
     @IBAction func closeButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
