b17b15be78b8308c4fc8fddde5b86e60eb2439f4
Eduard Guerra <eaguerra@students.tulsatech.edu>
2019-12-09 10:25:17 -0600

    Added Subtitle to annotations

--- Treasure Hunt/MapViewController.swift
+++ Treasure Hunt/MapViewController.swift
@@ -24,19 +24,22 @@ class MapViewController: UIViewController {
     let locationManager = CLLocationManager()
     
     let annotationLocations = [
-        ["title": "Riverside Cache", "latitude": 36.033846, "longitude": -95.981797],
-        ["title": "Peoria Cache", "latitude": 36.210999, "longitude": -95.976343],
-        ["title": "Broken Arrow Cache", "latitude": 36.003395, "longitude": -95.833936],
-        ["title": "Sand Spring Cache", "latitude": 36.137097, "longitude": -96.096525],
-        ["title": "Lemley Memorial Cache", "latitude": 36.113433, "longitude": -95.886927],
-        ["title": "Gathering Place Cache", "latitude": 36.122296, "longitude": -95.985425],
-        ["title": "Owen Park Cache", "latitude": 36.160408, "longitude": -96.005639],
-        ["title": "Gilcrease Museum Cache", "latitude": 36.174719, "longitude": -96.020289],
+        ["title": "Riverside Cache", "latitude": 36.033846, "longitude": -95.981797, "subtitle": "36.1370970,-96.0965250"],
+        ["title": "Peoria Cache", "latitude": 36.210999, "longitude": -95.976343, "subtitle": "36.210999,-95.976343"],
+        ["title": "Broken Arrow Cache", "latitude": 36.003395, "longitude": -95.833936, "subtitle": "36.003395,-95.833936"],
+        ["title": "Sand Spring Cache", "latitude": 36.137097, "longitude": -96.096525, "subtitle": "36.137097,-96.096525"],
+        ["title": "Lemley Memorial Cache", "latitude": 36.113433, "longitude": -95.886927, "subtitle": "36.113433,-95.886927"],
+        ["title": "Gathering Place Cache", "latitude": 36.122296, "longitude": -95.985425, "subtitle": "36.122296,-95.985425"],
+        ["title": "Owen Park Cache", "latitude": 36.160408, "longitude": -96.005639, "subtitle": "36.160408,-96.005639"],
+        ["title": "Gilcrease Museum Cache", "latitude": 36.174719, "longitude": -96.020289, "subtitle": "36.174719,-96.020289"],
+        ["title": "Dog Park Cache", "latitude": 36.016921, "longitude": -95.983280, "subtitle": "36.016921,-95.983280"],
+        
     ]
           
     func createAnnotations(locations: [[String : Any]]) {
         for location in locations {
             let annotations = MKPointAnnotation()
+            annotations.subtitle = location["subtitle"] as? String
             annotations.title = location["title"] as? String
             annotations.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees, longitude: location["longitude"] as! CLLocationDegrees)
             mapView.addAnnotation(annotations)
